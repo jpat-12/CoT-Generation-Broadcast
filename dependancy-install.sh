4f61adf26fe507f9e4d409678c8239c6b94f40d7
jpat-12 <79032412+jpat-12@users.noreply.github.com>
2023-11-30 23:30:20 -0600

    Update dependancy-install.sh

--- dependancy-install.sh
+++ dependancy-install.sh
@@ -46,22 +46,20 @@ if [[ $choice == "y" ]]; then
   echo "Is $Absolute_path the right path to update? (y/n) "
   read pathyn
   if [[ $pathyn == "y" ]]; then
-  
-    #change the absolute file path if apache is already installed
-    sed -i "s|curl -L \"with open('/var/www/html/COT-BROADCAST.xml', 'w') as xml_file:\"|curl -L \"with open('$Absolute_path/COT-BROADCAST.xml', 'w') as xml_file:\"|" /opt/cot-gen/convert-csv.py
-    #Verify Changes to output file path in convert-csv.py 
-    echo "Do you use Nano (1) or Vim (2)"
-    read nv
-
-    if [[ $nv == "1" ]]; then
-      nano /opt/cot-gen/convert-csv.py
-
-    elif [[ $nv == "2" ]]; then  
-      vi /opt/cot-gen/convert-csv.py
-    fi
-   # If the Absolute File Path is wrong than exit the script
-elif [[ $pathyn == "n" ]]; then  
-  exit
+      #change the absolute file path if apache is already installed
+      sed -i "s|curl -L \"with open('/var/www/html/COT-BROADCAST.xml', 'w') as xml_file:\"|curl -L \"with open('$Absolute_path/COT-BROADCAST.xml', 'w') as xml_file:\"|" /opt/cot-gen/convert-csv.py
+      #Verify Changes to output file path in convert-csv.py 
+      echo "Do you use Nano (1) or Vim (2)"
+      read nv
+      if [[ $nv == "1" ]]; then
+        nano /opt/cot-gen/convert-csv.py
+      elif [[ $nv == "2" ]]; then  
+       vi /opt/cot-gen/convert-csv.py
+      fi
+     # If the Absolute File Path is wrong than exit the script
+    elif [[ $pathyn == "n" ]]; then  
+      exit
+   fi
 fi
 
 
